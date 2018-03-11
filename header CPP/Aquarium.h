c5dfeb95e53ca1265b1e037927f015b688c62a58
Ahmad Faiz Sahupala <faiz.sahupala29@gmail.com>
2018-03-11 18:18:39 +0700

    add aquarium

---
+++ header CPP/Aquarium.h
@@ -0,0 +1,43 @@
+#ifndef AQUARIUM_H
+#define AQUARIUM_H
+#include <iostream>
+
+#define MAX_LENGTH 100
+#define MAX_WIDTH 100
+
+class Aquarium {
+private:
+  char[][] map = new char[MAX_LENGTH][MAX_WIDTH];
+  LinkedList<Guppy> listGuppy;
+  LinkedList<Piranha> listPiranha;
+  LinkedList<Coin> listCoin;
+  LinkedList<FishFood> listFishFood;
+  Snail snail;
+  int length;
+  int width;
+
+  Coordinate randomCoordinate();
+
+  void initiateGuppy();
+  void initiatePiranha();
+  void initiateSnail();
+
+public:
+  static int aquariumCoin = 10;
+
+  Aquarium();
+  Aquarium(int length, int width);
+  ~Aquarium();
+
+  void initiateMap();
+
+  void nextLifetime();
+
+  void addGuppy();
+  void addPiranha();
+  void addCoin();
+  void addFishFood();
+
+};
+
+#endif
